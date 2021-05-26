989262cacb6591e7af89ce2aeaaab9aacfd56646
Baracuda1337 <60934310+Baracuda1337@users.noreply.github.com>
2021-05-26 18:35:19 +0300

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,9 @@
+  const toggleContactUS = () => {
+    document.getElementById("ContactUS").classList.toggle("ContactUSToggled");
+  };
+  const toggle = () => {
+    document.getElementById("line1").classList.toggle("toggle_line1");
+    document.getElementById("line2").classList.toggle("toggle_line2");
+    document.getElementById("line3").classList.toggle("toggle_line3");
+    document.getElementById("burger_nav").classList.toggle("toggle_nav");
+  };
